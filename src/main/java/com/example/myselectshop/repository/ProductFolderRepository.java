bb14a4b6f508885a99aa04656a6a28c9ea185954
youngsik823 <youngsik823@naver.com>
2024-11-04 21:27:46 +0900

    상품 폴더 설계

---
+++ src/main/java/com/example/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package com.example.myselectshop.repository;
+
+import com.example.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+}
